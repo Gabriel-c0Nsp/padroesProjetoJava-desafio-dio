16b1eca3c7635f182579de605e096f95bbeca46e
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-10-18 12:23:31 -0300

    Criação do projeto para o desafio de código proposto na aula

---
+++ desafio/dio-viagens/src/main/java/one/digitalinnovation/dioviagens/DioViagensApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.dioviagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioViagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioViagensApplication.class, args);
+	}
+
+}
---
+++ desafio/dio-viagens/src/test/java/one/digitalinnovation/dioviagens/DioViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.dioviagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioViagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
